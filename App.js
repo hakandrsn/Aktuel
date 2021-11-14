c13ffd75954fcfcffe86e4a27c17342ddda27c2e
Hakan D <68669489+hakandrsn@users.noreply.github.com>
2021-11-14 23:04:20 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import App1 from './src/App'
+import { createStore } from 'redux'
+import { Provider } from "react-redux"
+
+
+
+const App = () => {
+  return (
+    <App1 />
+  )
+}
+
+export default App
